5d1ea9d641882d3b367797251fb85f27a087de78
moxingwang <xingwang.mo@chinaredstar.com>
2017-06-04 23:01:48 +0800

    mo

---
+++ src/main/java/com/mo/redis/mapper/MyBatisTest.java
@@ -0,0 +1,7 @@
+package com.mo.redis.mapper;
+
+/**
+ * Created by MoXingwang on 2017/6/4.
+ */
+public interface MyBatisTest {
+}
---
+++ src/test/java/com/test/BaseTest.java
@@ -0,0 +1,7 @@
+package com.test;
+
+/**
+ * Created by MoXingwang on 2017/6/4.
+ */
+public class BaseTest {
+}
